43a6080b96296c5adc00577973cabe428c675151
Edouard <edouard.maurice@edu.devinci.fr>
2023-03-16 17:23:40 +0100

    restriction measure actions

--- src/mesures/mesure.controller.js
+++ src/mesures/mesure.controller.js
@@ -14,55 +14,6 @@ router.post(
     controllerCreateOneMesure
 );
 
-async function controllerGetAllMesure(req,res){
-    const limit = req.query.limit || 20;
-    const offset = req.query.offset || 0;
-    const mesure = await mesureService.findAll(limit, offset);
-    return res.status(200).send(mesure);
-}
-
-router.get("/", controllerGetAllMesure);
-
-async function controllerGetOneMesure(req, res, next){
-    try{
-        const mesure = await mesureService.findOne(req.params.id);
-        return res.status(200).send(mesure);
-    } catch(err){
-       return httpErrorHelper(err, req, res, next);
-    }
-}
-
-router.get("/:id", controllerGetOneMesure);
-
-async function controllerUpdateOneMesure(req, res, next){
-    try{
-        const mesure = await mesureService.updateOne(req.params.id, req.body);
-        return res.status(200).send(mesure);
-    } catch(err){
-        return httpErrorHelper(err, req, res, next);
-    }
-}
-
-router.patch(
-    "/:id",
-    authorizationMiddleware.canAccess(["admin"]),
-    controllerUpdateOneMesure
-);
-
-async function controllerDeleteOneMesure(req, res, next){
-    try{
-        const mesure = await mesureService.deleteOne(req.params.id);
-        return res.status(200).send(mesure);
-    }catch(err){
-        httpErrorHelper(err, req, res, next)
-    }
-}
-router.delete(
-    ":/id",
-    authorizationMiddleware.canAccess(["admin"]),
-    controllerDeleteOneMesure
-);
-
 async function getLastMesure(req, res, next){
     try{
         const mesure = await mesureService.findRecentOne();
@@ -72,6 +23,6 @@ async function getLastMesure(req, res, next){
     }
 }
 
-router.get("/last/last",getLastMesure);
+router.get("/last",getLastMesure);
 
 module.exports = router;
